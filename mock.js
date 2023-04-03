f1d4db6c9f397b3ba2c01304381d5084870659f6
Milo Bravman <Milobravman20@gmail.com>
2023-04-03 13:00:41 -0400

    adding icons

--- mock.js
+++ mock.js
@@ -12,18 +12,35 @@ function loadImages(numImages = 10){
     //  })
     //  i++;
     // }
-    container = document.getElementById('cardZone');
-    console.log(container)
+    container = document.getElementById('cardZone')
+    
+
+    //creates img htlm element
     const img = document.createElement('img')
     img.src = './food.jpg'
-    container.appendChild(img)
+    img.sizes = 'small'
+
+    //creates title html element
+    const title = document.createElement('h4')
+    const node = document.createTextNode("[Food] Name")
+    title.appendChild(node)
+
+    //creates div
+    const div = document.createElement('div')
+    div.className = 'media'
+
+    
+
+    div.appendChild(title)
+    div.appendChild(img)
+    container.appendChild(div)
    }
  
  //loadImages();
 
  window.addEventListener('scroll',()=>{
-    console.log(window.scrollY) //scrolled from top
-    console.log(window.innerHeight) //visible part of screen
+    //console.log(window.scrollY) //scrolled from top
+    //console.log(window.innerHeight) //visible part of screen
     if(window.scrollY + window.innerHeight >= 
     document.documentElement.scrollHeight){
     loadImages();
